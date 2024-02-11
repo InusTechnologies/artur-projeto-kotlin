2803a5ed6589a5ca002d7f5a8a64cf0d518a9eb3
inustec <inustec@gmail.com>
2024-02-11 08:15:00 -0300

    First steps in kotlin with Spring boot

---
+++ 05A_firstStepsinKotlinWithSpringBoot/rest-with-spring-boot-and-kotlin-artur/src/main/kotlin/br/com/artur/Startup.kt
@@ -0,0 +1,11 @@
+package br.com.artur
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestWithSpringBootAndKotlinArturApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestWithSpringBootAndKotlinArturApplication>(*args)
+}
---
+++ 05B_firstStepsinKotlinWithSpringBoot - Copia/rest-with-spring-boot-and-kotlin-artur/src/main/kotlin/br/com/artur/Greeting.kt
@@ -0,0 +1,3 @@
+package br.com.artur;
+
+class Greeting (val id: Long, val content: String)
